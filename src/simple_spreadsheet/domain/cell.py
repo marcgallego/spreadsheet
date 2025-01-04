778b15dc545d1512ab4d43c84c1174b651144fb0
Marc Gállego <mga2525@gmail.com>
2025-01-04 18:24:57 +0100

    don't include coords in Cell for the moment + new getters

--- src/simple_spreadsheet/domain/cell.py
+++ src/simple_spreadsheet/domain/cell.py
@@ -1,14 +1,16 @@
 from .contents import Content
-from .coordinates import Coordinates
 
 
 class Cell:
-    def __init__(self, coords: Coordinates) -> None:
-        self.coordinates = coords
+    def __init__(self) -> None:
+        # TODO: decide if the identifier is needed
         self.content = None
 
-    def set_value(self, content) -> None:
+    def set_value(self, content: Content) -> None:
         self.content = content
 
-    def get_value(self) -> None | Content:
+    def get_content(self) -> None | Content:
         return self.content
+
+    def get_value(self) -> None | float:
+        return self.content.get_value() if self.content else None
