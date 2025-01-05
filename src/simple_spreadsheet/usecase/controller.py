df49bdd3c055c19c3c80a9f66bcc4cb4eceba23a
Marc Gállego <mga2525@gmail.com>
2025-01-05 19:59:15 +0100

    use ContentFactory

--- src/simple_spreadsheet/usecase/controller.py
+++ src/simple_spreadsheet/usecase/controller.py
@@ -1,11 +1,14 @@
 from domain.spreadsheet import Spreadsheet
-from domain.contents import Number
+from domain.contents import ContentFactory
 from framework.ui import UserInterface
 
 
 class Controller:
     def __init__(self) -> None:
         self._spreadsheet = Spreadsheet()
-        self._spreadsheet.edit_cell('A1', Number(5))
+
+        self._spreadsheet.edit_cell('A1', ContentFactory.create('hola'))
+        self._spreadsheet.edit_cell('B5', ContentFactory.create('5'))
+        self._spreadsheet.edit_cell('C3', ContentFactory.create('=B5+5'))
         self._ui = UserInterface(self._spreadsheet)
         self._ui.run()
