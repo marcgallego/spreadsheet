1f5bd4eb4e1a53ddd5581849e3722f7f509ba77c
Marc Gállego <mga2525@gmail.com>
2025-01-06 11:50:06 +0100

    fixed first column in UI + use row_name as key

--- src/simple_spreadsheet/framework/ui.py
+++ src/simple_spreadsheet/framework/ui.py
@@ -14,6 +14,7 @@ class UserInterface(App):
 
         cols = self.spreadsheet.get_columns()
         table.add_columns("", *cols)
+        table.fixed_columns = 1
 
         rows = self.spreadsheet.get_rows()
         vals = self.spreadsheet.get_all_values()
@@ -22,6 +23,6 @@ class UserInterface(App):
         for i in range(n):
             row_name = rows[i]
             row_vals = vals[i]
-            table.add_row(row_name, *row_vals)
+            table.add_row(row_name, *row_vals, key=row_name)
 
         yield table
