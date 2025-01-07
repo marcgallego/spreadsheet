6771b9232867fb78a60e025224ff68d2c09fd47f
Marc Gállego <mga2525@gmail.com>
2025-01-07 01:48:33 +0100

    formula evaluation working (1st version)

--- src/simple_spreadsheet/domain/formula_evaluation/main.py
+++ src/simple_spreadsheet/domain/formula_evaluation/main.py
@@ -1,6 +1,9 @@
 from .tokenizer import Tokenizer
 from .validator import Validator
 from .parser import Parser
+from .postfix_evaluator import PostfixEvaluator
+
+from ..spreadsheet import Spreadsheet
 
 
 class FormulaEvaluator:
@@ -8,8 +11,9 @@ class FormulaEvaluator:
         self._tokenizer = Tokenizer()
         self._validator = Validator()
         self._parser = Parser()
+        self._postfix_evaluator = PostfixEvaluator()
 
-    def evaluate(self, formula: str) -> float:
+    def evaluate(self, formula: str, spreadsheet: Spreadsheet) -> float:
         tokens = self._tokenizer.tokenize(formula)
         print(tokens)
         self._validator.has_syntax_error(tokens)
@@ -17,3 +21,5 @@ class FormulaEvaluator:
         print(components)
         postfix = self._parser.infix_to_postfix(components)
         print(postfix)
+        result = self._postfix_evaluator.evaluate(postfix, spreadsheet)
+        print(result)
---
+++ src/simple_spreadsheet/domain/formula_evaluation/postfix_evaluator.py
@@ -0,0 +1,28 @@
+from ..spreadsheet import Spreadsheet
+from ..coordinates import Coordinates
+from ..formula_component import ComponentType, FormulaComponent
+
+
+class PostfixEvaluator:
+    def evaluate(self, postfix: list[FormulaComponent], spreadsheet: Spreadsheet) -> float:
+        stack = []
+        for component in postfix:
+
+            component_type = component.get_type() if not isinstance(
+                component, float) else ComponentType.OPERAND
+
+            if component_type == ComponentType.OPERAND:
+                stack.append(component)
+            elif component_type == ComponentType.OPERATOR:
+                # Pop the required number of operands for the operator
+                operand2 = stack.pop()
+                operand1 = stack.pop()
+
+                result = component.operate(operand1, operand2, spreadsheet)
+                stack.append(result)
+
+        if len(stack) != 1:
+            raise ValueError(
+                "Invalid postfix expression: stack does not contain exactly one element.")
+
+        return stack.pop()
