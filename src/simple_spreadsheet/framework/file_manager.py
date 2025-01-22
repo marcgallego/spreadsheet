f555fb54a5af0641c912e36d5e7af06222e00da0
Marc Gállego <mga2525@gmail.com>
2025-01-22 20:12:44 +0100

    add exceptions for saving and reading

--- src/simple_spreadsheet/framework/file_manager.py
+++ src/simple_spreadsheet/framework/file_manager.py
@@ -1,25 +1,44 @@
-from simple_spreadsheet.domain.spreadsheet import Spreadsheet
-from simple_spreadsheet.domain.contents import ContentFactory
-from simple_spreadsheet.domain.coordinates import Coordinates
+from ..domain.spreadsheet import Spreadsheet
+from ..domain.contents import ContentFactory
+from ..domain.coordinates import Coordinates
+
+
+class SavingSpreadsheetException(Exception):
+    def __init__(self) -> None:
+        Exception.__init__(self)
+
+    def __init__(self, msg) -> None:
+        Exception.__init__(self, msg)
+
+
+class ReadingSpreadsheetException(Exception):
+    def __init__(self) -> None:
+        Exception.__init__(self)
+
+    def __init__(self, msg) -> None:
+        Exception.__init__(self, msg)
 
 
 class FileManager:
     def read(self, file_path: str) -> Spreadsheet:
         """Reads the content of a file and returns a Spreadsheet object."""
-        spreadsheet = Spreadsheet()
-        coords_with_formulas: list[Coordinates] = []
-        with open(file_path, "r") as file:
-            for row_index, line in enumerate(file):
-                row = line.strip().split(";")
-                for col_index, cell_data in enumerate(row):
-                    if cell_data != "":
-                        coords = Coordinates(row_index, col_index)
-                        if cell_data[0] == '=':
-                            cell_data = cell_data.replace(",", ";")
-                            coords_with_formulas.append(coords)
-                        content = ContentFactory.create(cell_data)
-                        spreadsheet.set_content(coords, content)
-        return spreadsheet, coords_with_formulas
+        try:
+            spreadsheet = Spreadsheet()
+            coords_with_formulas: list[Coordinates] = []
+            with open(file_path, "r") as file:
+                for row_index, line in enumerate(file):
+                    row = line.strip().split(";")
+                    for col_index, cell_data in enumerate(row):
+                        if cell_data != "":
+                            coords = Coordinates(row_index, col_index)
+                            if cell_data[0] == '=':
+                                cell_data = cell_data.replace(",", ";")
+                                coords_with_formulas.append(coords)
+                            content = ContentFactory.create(cell_data)
+                            spreadsheet.set_content(coords, content)
+            return spreadsheet, coords_with_formulas
+        except Exception as e:
+            raise ReadingSpreadsheetException(str(e))
 
     def _write_file(self, file_path: str, output: list[list[str]]) -> None:
         """Writes the processed content to a file."""
@@ -29,29 +48,32 @@ class FileManager:
 
     def save(self, spreadsheet: Spreadsheet, file_path: str) -> None:
         """Saves a Spreadsheet object to a file."""
-        cells = spreadsheet.cells
-        output = []
-        last_non_empty_row = -1
+        try:
+            cells = spreadsheet.cells
+            output = []
+            last_non_empty_row = -1
 
-        for row_index, row in enumerate(cells):
-            row_output = []
-            last_non_empty_col = -1
+            for row_index, row in enumerate(cells):
+                row_output = []
+                last_non_empty_col = -1
 
-            for col_index, cell in enumerate(row):
-                cell_output = cell.get_value_to_dump()
-                row_output.append(cell_output)
-                if cell_output != "":
-                    last_non_empty_col = col_index
+                for col_index, cell in enumerate(row):
+                    cell_output = cell.get_value_to_dump()
+                    row_output.append(cell_output)
+                    if cell_output != "":
+                        last_non_empty_col = col_index
 
-            # Trim to the last non-empty column
-            if last_non_empty_col >= 0:
-                row_output = row_output[:last_non_empty_col + 1]
-                last_non_empty_row = row_index
-            else:
-                row_output = []  # Skip empty rows
+                # Trim to the last non-empty column
+                if last_non_empty_col >= 0:
+                    row_output = row_output[:last_non_empty_col + 1]
+                    last_non_empty_row = row_index
+                else:
+                    row_output = []  # Skip empty rows
 
-            output.append(row_output)
+                output.append(row_output)
 
-        # Trim to the last non-empty row
-        output = output[:last_non_empty_row + 1]
-        self._write_file(file_path, output)
+            # Trim to the last non-empty row
+            output = output[:last_non_empty_row + 1]
+            self._write_file(file_path, output)
+        except Exception as e:
+            raise SavingSpreadsheetException(str(e))
