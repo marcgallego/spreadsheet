73cb7ccb6cd043635dd7b7422f1cea30aeb7c738
Marc Gállego <mga2525@gmail.com>
2025-01-22 18:57:12 +0100

    use visitor for the converter

--- src/simple_spreadsheet/domain/formula_evaluation/converter.py
+++ src/simple_spreadsheet/domain/formula_evaluation/converter.py
@@ -1,40 +1,44 @@
-from ..formula_component import FormulaComponent, ComponentType
+from ..formula_component import FormulaComponent
 from ..operators import BinaryOperator
 from .visitor import Visitor
-from .consts import PRECEDENCE, OPENING_PARENTHESIS
+from .consts import PRECEDENCE
 
 
-class Converter():
+class Converter(Visitor):
 
     def __init__(self) -> None:
+        self._output = []
+        self._stack = []
         self._precedence = PRECEDENCE
-        self._opening_parenthesis = OPENING_PARENTHESIS
+        self._opening_parenthesis = '('
+
+    def visit_operand(self, operand: FormulaComponent) -> None:
+        self._output.append(operand)
+
+    def visit_operator(self, operator: FormulaComponent) -> None:
+        while (self._stack and isinstance(self._stack[-1], BinaryOperator) and
+               self._precedence.get(self._stack[-1].symbol, 0) >= self._precedence.get(operator.symbol, 0)):
+            self._output.append(self._stack.pop())
+        self._stack.append(operator)
+
+    def visit_opening_parenthesis(self, parenthesis: FormulaComponent) -> None:
+        self._stack.append(parenthesis)
+
+    def visit_closing_parenthesis(self, _: FormulaComponent) -> None:
+        while self._stack and self._stack[-1].symbol != self._opening_parenthesis:
+            self._output.append(self._stack.pop())
+        if self._stack and self._stack[-1].symbol == self._opening_parenthesis:
+            self._stack.pop()
 
     def infix_to_postfix(self, components: list[FormulaComponent]) -> list[FormulaComponent]:
         """Converts a list of Components in infix order to postfix order."""
-        output = []
-        stack = []
-        precedence = self._precedence
+        self._output = []
+        self._stack = []
 
         for component in components:
-            component_type = component.type
+            component.accept(self)
 
-            if component_type == ComponentType.OPERAND:
-                output.append(component)
-            elif component_type == ComponentType.OPERATOR:
-                while (stack and isinstance(stack[-1], BinaryOperator) and
-                       precedence.get(stack[-1].symbol, 0) >= precedence.get(component.symbol, 0)):
-                    output.append(stack.pop())
-                stack.append(component)
-            elif component_type == ComponentType.OPENING_PARENTHESIS:
-                stack.append(component)
-            elif component_type == ComponentType.CLOSING_PARENTHESIS:
-                while stack and stack[-1].symbol != self._opening_parenthesis:
-                    output.append(stack.pop())
-                if stack and stack[-1].symbol == self._opening_parenthesis:
-                    stack.pop()
+        while self._stack:
+            self._output.append(self._stack.pop())
 
-        while stack:
-            output.append(stack.pop())
-
-        return output
+        return self._output
